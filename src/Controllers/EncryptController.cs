c4551bf2daa23a0faa84b53800f7958b10c1c2e1
Shai Katz <shai@soluto.com>
2018-07-08 13:41:00 +0300

    - Remove debugging code - Add logs

--- src/Controllers/EncryptController.cs
+++ src/Controllers/EncryptController.cs
@@ -12,9 +12,11 @@ using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Rest;
-using Microsoft.Extensions.Logging;
 using System.Security.Cryptography;
+using Hamuste.Extensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.WebUtilities;
+using Serilog;
 
 namespace Hamuste.Controllers
 {
@@ -24,9 +26,11 @@ namespace Hamuste.Controllers
         private readonly IKubernetes mKubernetes;
         private readonly IKeyVaultClient mKeyVaultClient;
         private readonly IAuthorizationService mAuthorizationService;
+        private readonly IHttpContextAccessor mHttpContextAccessor;
         private readonly string mKeyVaultName;
         private readonly string mKeyType;
-        private readonly ILogger<EncryptController> mLogger;
+        private readonly ILogger mAuditLogger = Log.ForContext<EncryptController>().AsAudit();
+        private readonly ILogger mLogger = Log.ForContext<EncryptController>();
 
         //see: https://github.com/kubernetes/kubernetes/blob/d5803e596fc8aba17aa8c74a96aff9c73bb0f1da/staging/src/k8s.io/apiserver/pkg/authentication/serviceaccount/util.go#L27
         private const string ServiceAccountUsernamePrefix = "system:serviceaccount:";
@@ -35,18 +39,16 @@ namespace Hamuste.Controllers
             IKubernetes kubernetes, 
             IKeyVaultClient keyVaultClient,
             IAuthorizationService authorizationService,
-            IConfiguration configuration,
-            ILogger<EncryptController> logger)
+            IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration)
         {
             mKubernetes = kubernetes;
             mKeyVaultClient = keyVaultClient;
             mAuthorizationService = authorizationService;
-            var config = string.Join(Environment.NewLine, configuration.AsEnumerable().Where(i => !i.Key.ToLower().Contains("secret")).Select(i => $"{i.Key} => {i.Value}"));
-
-            Console.WriteLine($"Configuration on controller: {Environment.NewLine} {config}");
+            mHttpContextAccessor = httpContextAccessor;
+            
             mKeyVaultName = configuration["KeyVault:Name"];
             mKeyType = configuration["KeyVault:KeyType"];
-            mLogger = logger;
         }
 
         [HttpPost]
@@ -58,18 +60,17 @@ namespace Hamuste.Controllers
             try
             {
                 serviceAccount = await mKubernetes.ReadNamespacedServiceAccountAsync(body.SerivceAccountName, body.NamesapceName, true);
+                mAuditLogger.Information("Encryption request started, SourceIP: {sourceIp}, ServiceAccount: {sa}", 
+                    Request.HttpContext.Connection.RemoteIpAddress,
+                    body.SerivceAccountName);
             }
             catch (HttpOperationException e) when (e.Response.StatusCode == HttpStatusCode.NotFound)
             {
+                mLogger.Warning(e, "Encryption request failed, SourceIP: {sourceIp}, ServiceAccount: {sa}", 
+                    Request.HttpContext.Connection.RemoteIpAddress,
+                    body.SerivceAccountName);                
                 return BadRequest();
             }
-            catch (HttpOperationException e)
-            {
-                Console.WriteLine($"Error: content - {e.Response.Content}");
-                Console.WriteLine($"Error: reason - {e.Response.ReasonPhrase}");
-
-                throw;
-			}
 
             var id = $"{body.NamesapceName}:{body.SerivceAccountName}";
             var hash = ComputeKeyId(id);
@@ -80,25 +81,37 @@ namespace Hamuste.Controllers
 
             try
             {
-                var key = await mKeyVaultClient.GetKeyAsync(keyId);
-            }catch (KeyVaultErrorException e) when (e.Response.StatusCode == HttpStatusCode.NotFound){
+                await mKeyVaultClient.GetKeyAsync(keyId);
+            }
+            catch (KeyVaultErrorException e) when (e.Response.StatusCode == HttpStatusCode.NotFound)
+            {
                 await mKeyVaultClient.CreateKeyAsync($"https://{mKeyVaultName}.vault.azure.net", hash, mKeyType, 2048);
             }
+
             var encryptionResult = await mKeyVaultClient.EncryptAsync(keyId, "RSA-OAEP", Encoding.UTF8.GetBytes(body.Data));
 
+            mAuditLogger.Information("Encryption request succeeded, SourceIP: {sourceIp}, ServiceAccount: {sa}", 
+                Request.HttpContext.Connection.RemoteIpAddress,
+                body.SerivceAccountName);
             return Content(Convert.ToBase64String(encryptionResult.Result));
         }
 
         [HttpPost]
         [Route("api/v1/decrypt")]
         [Authorize(AuthenticationSchemes = "kubernetes")]
-        public async Task<ActionResult> Decrypt([FromBody]DecryptRequest body)
+        public async Task<ActionResult> Decrypt([FromBody] DecryptRequest body)
         {
             var serviceAccountUserName = User.Claims.FirstOrDefault(claim => claim.Type == "name")?.Value;
 
-            if (string.IsNullOrEmpty(serviceAccountUserName) || !serviceAccountUserName.StartsWith(ServiceAccountUsernamePrefix, StringComparison.InvariantCulture)){
+            if (string.IsNullOrEmpty(serviceAccountUserName) ||
+                !serviceAccountUserName.StartsWith(ServiceAccountUsernamePrefix, StringComparison.InvariantCulture))
+            {
                 return StatusCode(403);
             }
+
+            mAuditLogger.Information("Decryption request started, SourceIP: {sourceIp}, ServiceAccount: {sa}",
+                Request.HttpContext.Connection.RemoteIpAddress,
+                serviceAccountUserName);
 
             var id = serviceAccountUserName.Replace(ServiceAccountUsernamePrefix, "");
             var hash = ComputeKeyId(id);
@@ -106,11 +119,16 @@ namespace Hamuste.Controllers
             var keyId = $"https://{mKeyVaultName}.vault.azure.net/keys/{hash}";
             try
             {
-                var encryptionResult = await mKeyVaultClient.DecryptAsync(keyId, "RSA-OAEP", Convert.FromBase64String(body.EncryptedData));
+                var encryptionResult =
+                    await mKeyVaultClient.DecryptAsync(keyId, "RSA-OAEP", Convert.FromBase64String(body.EncryptedData));
 
                 return Content(Encoding.UTF8.GetString(encryptionResult.Result));
-            } catch (KeyVaultErrorException e) {
-                Console.WriteLine(e);
+            }
+            catch (KeyVaultErrorException e)
+            {
+                mLogger.Warning(e, "Decryption request failed, SourceIP: {sourceIp}, ServiceAccount: {sa}",
+                    Request.HttpContext.Connection.RemoteIpAddress,
+                    serviceAccountUserName);
                 return StatusCode(400);
             }
         }
