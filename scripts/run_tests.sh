e3bac91414a80304da527e9403cb23144e3731d5
Omer Levi Hevroni <omerlh@gmail.com>
2018-07-16 07:31:55 +0300

    improve docker caching

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,5 +9,5 @@ then
     api_file="docker-compose.local.yaml"
 fi
 
-docker-compose -f tests/blackbox/compose/docker-compose.yaml -f tests/blackbox/compose/$api_file -f tests/blackbox/compose/docker-compose.security.yaml pull --parallel
-docker-compose -f tests/blackbox/compose/docker-compose.yaml -f tests/blackbox/compose/$api_file -f tests/blackbox/compose/docker-compose.security.yaml up --build --exit-code-from black-box --abort-on-container-exit
+docker-compose -f tests/blackbox/compose/docker-compose.yaml -f tests/blackbox/compose/$api_file pull --parallel
+docker-compose -f tests/blackbox/compose/docker-compose.yaml -f tests/blackbox/compose/$api_file up --build --exit-code-from black-box --abort-on-container-exit
