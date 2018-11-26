547f421905abf6cdf38beed6e408ec12c663d4df
Shai Katz <shai@soluto.com>
2018-11-26 16:27:34 +0200

    add files support for init decryptor

--- init-decryptor/tests/run_test.sh
+++ init-decryptor/tests/run_test.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "Removing otput directory"
+
+rm -rf output
+
 echo "starting wiremock"
 
 docker-compose up -d --build wiremock
@@ -12,14 +16,32 @@ echo "running decryptor - json format"
 
 OUTPUT_FORMAT=json docker-compose run decryptor
 
-echo "comparing files"
+echo "comparing out.json and expected.json files"
 
-diff -q out.json expected.json
+diff -q output/out.json expected.json
+
+rm -rf output
 
 echo "running decryptor - cfg format"
 
 OUTPUT_FORMAT=cfg docker-compose run decryptor
 
-echo "comparing files"
+echo "comparing out.cfg and expected.cfg files"
 
-diff -q out.cfg expected.cfg
+diff -q output/out.cfg expected.cfg
+
+rm -rf output
+
+echo "running decryptor - files format"
+
+OUTPUT_FORMAT=files docker-compose run decryptor
+
+echo "comparing output directory with expected.json"
+
+for f in "output"/*; do
+  output=$(cat "$f")
+  expected=$(cat expected.json | jq .$(basename $f) | sed -e 's/^"//' -e 's/"$//') # remove double qoutes because they doesn't exist in files
+  diff <(echo $output) <(echo $expected)
+done
+
+rm -rf output
