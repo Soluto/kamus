917acbf3d608d8ea7436c0fb64d0a0ef75d3ca68
omerlh <omerl@soluto.com>
2019-01-21 20:00:44 +0200

    use caporal logger

--- cli/index.js
+++ cli/index.js
@@ -4,8 +4,22 @@ const pjson = require('./package.json');
 const prog = require('caporal');
 const encrypt = require('./actions/encrypt');
 const regexGuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const bunyan = require('bunyan');
+const PrettyStream = require('bunyan-pretty-colors');
+ 
+var prettyStdOut = new PrettyStream();
+prettyStdOut.pipe(process.stdout);
+ 
+const { ColorfulChalkLogger, DEBUG } = require('colorful-chalk-logger')
+ 
+const logger = new ColorfulChalkLogger('kamus-cli', {
+  level: DEBUG,   // the default value is INFO
+  date: false,    // the default value is false.
+  colorful: true, // the default value is true.
+}, process.argv)
 
 prog
+  .logger(logger)
   .version(pjson.version)
   .command('encrypt', 'Encrypt data')
   .action(encrypt)
@@ -21,6 +35,10 @@ prog
   .option('--cert-fingerprint <certFingerprint>', 'Force server certificate to match the given fingerprint', prog.STRING)
   .option('--secret-file-encoding <fileEncoding>', 'Encoding of secret file', prog.STRING)
   .option('-o, --output <outputFile>', 'Output to file', prog.STRING)
-  .option('-O, --overwrite', 'Overwrites file if it already exists', prog.BOOL);
+  .option('-O, --overwrite', 'Overwrites file if it already exists', prog.BOOL)
+  .option('--log-level <debug|verbose|info|warn|error|fatal>', 'log level', prog.STRING)
+  .option('--log-flag <date|inline|colorful|no-date|no-inline|no-colorful>', 'log format', prog.STRING)
+  .option('--log-output <filepath>', 'output log to file', prog.STRING)
+  .option('--log-encoding <encoding>', 'log file encoding');
 
 prog.parse(process.argv);
