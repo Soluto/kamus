cd5910ce6f6a3671e7cf2fec4104246d8de2f40e
Shai Katz <shai@soluto.com>
2019-01-22 12:58:07 +0200

    fix package.json imports

--- cli/lib/index.js
+++ cli/lib/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const pjson = require('./package.json');
+const pjson = require('../package.json');
 const prog = require('caporal');
 const encrypt = require('./actions/encrypt');
 const regexGuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
