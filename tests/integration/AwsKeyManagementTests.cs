24f3eb34f08da5a2d6d69451393e98e0d286b9c0
Shai Katz <shai@soluto.com>
2019-03-03 17:32:15 +0200

    remove unused

--- tests/integration/AwsKeyManagementTests.cs
+++ tests/integration/AwsKeyManagementTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Amazon;
 using Amazon.KeyManagementService;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.CloudKMS.v1;
-using Google.Apis.Services;
 using Kamus.KeyManagement;
 using Microsoft.Extensions.Configuration;
 using Xunit;
@@ -23,13 +18,12 @@ namespace integration
                 .AddJsonFile("settings.json")
                 .AddEnvironmentVariables().Build();
 
-            var userArn = mConfiguration.GetValue<string>("KeyManagement:AwsKms:UserArn");
             var awsKey = mConfiguration.GetValue<string>("KeyManagement:AwsKms:Key");
             var awsSecret = mConfiguration.GetValue<string>("KeyManagement:AwsKms:Secret");
 
             var kmsService = new AmazonKeyManagementServiceClient(awsKey, awsSecret, RegionEndpoint.USEast1);
 
-            mAwsKeyManagement = new AwsKeyManagement(kmsService, new SymmetricKeyManagement(), userArn);
+            mAwsKeyManagement = new AwsKeyManagement(kmsService, new SymmetricKeyManagement());
         }
 
         [Fact]
