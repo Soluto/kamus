487ed76d73d596914e84e7e4d35e1b4e95779a38
omerlh <omerl@soluto.com>
2019-03-05 22:11:52 +0200

    wip

---
+++ src/crd-controller/Models/KamusSecret.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using k8s;
+using k8s.Models;
+
+namespace CustomResourceDescriptorController.Models
+{
+    public class KamusSecret : KubernetesObject
+    {
+        public Dictionary<string, string> Data { get; set; }
+        public string Type { get; set; }
+        public V1ObjectMeta Metadata { get; set; }
+
+    }
+}
---
+++ src/crd-controller/V1Alpha/KamusSecretController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using CustomResourceDescriptorController.Models;
+using k8s;
+using k8s.Models;
+using Kamus.KeyManagement;
+using Serilog;
+using CustomResourceDescriptorController.Extensions;
+
+namespace CustomResourceDescriptorController.V1Alpha.CustomResourceDescriptorController
+{
+    public class KamusSecretController : IDisposable
+    {
+        private readonly IKubernetes mKubernetes;
+        private readonly IKeyManagement mKeyManagement;
+        private IDisposable mSubscription;
+        private readonly ILogger mAuditLogger = Log.ForContext<KamusSecretController>().AsAudit();
+        private readonly ILogger mLogger = Log.ForContext<KamusSecretController>();
+
+        public KamusSecretController(IKubernetes kubernetes, IKeyManagement keyManagement)
+        {
+            this.mKubernetes = kubernetes;
+            this.mKeyManagement = keyManagement;
+        }
+
+        public void Dispose()
+        {
+            mSubscription.Dispose();
+        }
+
+        public void Listen()
+        {
+            mSubscription = Observable.FromAsync(async () =>
+            {
+                var result = await mKubernetes.ListClusterCustomObjectWithHttpMessagesAsync("soluto.com", "v1alpha1", "kamussecrets", watch: true);
+                var subject = new System.Reactive.Subjects.Subject<(WatchEventType, KamusSecret)>();
+
+                var watcher = result.Watch<KamusSecret>(
+                     onEvent: (@type, @event) => subject.OnNext((@type, @event)),
+                     onError: e => subject.OnError(e),
+                     onClosed: () => subject.OnCompleted());
+                return subject;
+            })
+            .SelectMany(x => x)
+            .Select(t => (t.Item1, t.Item2 as KamusSecret))
+            .Where(t => t.Item2 != null)
+            .SelectMany(x =>
+                Observable.FromAsync(async () => await HandleEvent(x.Item1, x.Item2))
+            )
+            .Subscribe(
+                onNext: t => { }, 
+                onError: e => mLogger.Error(e, "Unexpected error occured while watching KamusSecret events"), 
+                onCompleted: () => mLogger.Debug("Watching KamusSecret events completed"));
+
+            mLogger.Information("Starting watch for KamusSecret V1Alpha events");
+
+        }
+
+        private async Task HandleEvent(WatchEventType @event, KamusSecret kamusSecret)
+        {
+            mLogger.Information("Handling event of type {type}. KamusSecret {name} in namesapce {namespace}",
+                @event.ToString(),
+                kamusSecret.Metadata.Name,
+                kamusSecret.Metadata.NamespaceProperty ?? "default");
+
+            switch (@event)
+            {
+                case WatchEventType.Added:
+                    await HandleAdd(kamusSecret);
+                    return;
+
+                case WatchEventType.Deleted:
+                    await HandleDelete(kamusSecret);
+                    return;
+
+                default:
+                    mLogger.Warning("Event of type {type} is not supported. KamusSecret {name} in namesapce {namespace}",
+                        @event.ToString(),
+                        kamusSecret.Metadata.Name,
+                        kamusSecret.Metadata.NamespaceProperty ?? "default");
+                    return;
+
+            }
+        }
+
+        private async Task HandleAdd(KamusSecret kamusSecret)
+        {
+            var @namespace = kamusSecret.Metadata.NamespaceProperty ?? "default";
+            var serviceAccount = "default";
+            var id = $"{@namespace}:{serviceAccount}";
+
+            var decryptedItems = new Dictionary<string, string>();
+
+            mLogger.Debug("Starting decrypting KamusSecret items. KamusSecret {name} in namesapce {namespace}",
+                kamusSecret.Metadata.Name,
+                kamusSecret.Metadata.NamespaceProperty ?? "default");
+
+            foreach (var item in kamusSecret.Data)
+            {
+                try
+                {
+                    var decrypted = await mKeyManagement.Decrypt(item.Value, id);
+
+                    decryptedItems.Add(item.Key, decrypted);
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e, "Failed to decrypt KamusSecret key {key}. KamusSecret {name} in namesapce {namespace}",
+                        item.Key,
+                        kamusSecret.Metadata.Name,
+                        @namespace);
+
+                    var @event2 = new V1Event
+                    {
+                        Action = "Create secret from KamusSecret failed",
+                        InvolvedObject = new V1ObjectReference
+                        {
+                            Name = kamusSecret.Metadata.Name,
+                            NamespaceProperty = @namespace,
+                            Kind = kamusSecret.Kind,
+                            ApiVersion = kamusSecret.ApiVersion,
+                            ResourceVersion = kamusSecret.Metadata.ResourceVersion,
+                            Uid = kamusSecret.Metadata.Uid
+                        },
+                        Metadata = new V1ObjectMeta
+                        {
+                             NamespaceProperty = @namespace,
+                             Name = "KamusCreate"
+                        }
+
+                    };
+
+                    await mKubernetes.CreateNamespacedEventAsync(@event2, @namespace);
+                    return;
+                }
+            }
+
+
+            var secret = new V1Secret
+            {
+                Metadata = new V1ObjectMeta
+                {
+                    Name = kamusSecret.Metadata.Name,
+                    NamespaceProperty = @namespace
+                },
+                Type = kamusSecret.Type,
+                StringData = decryptedItems
+            };
+
+            await mKubernetes.CreateNamespacedSecretAsync(secret, @namespace);
+
+            var @event = new V1Event
+            {
+                Message = "Create secret from KamusSecret succeed",
+                InvolvedObject = new V1ObjectReference
+                {
+                    Name = kamusSecret.Metadata.Name,
+                    NamespaceProperty = @namespace,
+                    Kind = kamusSecret.Kind,
+                    ApiVersion = kamusSecret.ApiVersion,
+                    ResourceVersion = kamusSecret.Metadata.ResourceVersion,
+                    Uid = kamusSecret.Metadata.Uid
+                },
+                Metadata = new V1ObjectMeta
+                {
+                    NamespaceProperty = @namespace,
+                    Name = $"{Environment.MachineName}.{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}"
+                },
+                Type = "Success",
+                ReportingComponent = "KamusCRDController",
+
+            };
+
+            await mKubernetes.CreateNamespacedEventAsync(@event, @namespace);
+        }
+
+        private async Task HandleDelete(KamusSecret kamusSecret)
+        {
+            var @namespace = kamusSecret.Metadata.NamespaceProperty ?? "default";
+
+            await mKubernetes.DeleteNamespacedSecretAsync(
+                new V1DeleteOptions { }, 
+                kamusSecret.Metadata.Name, 
+                @namespace);
+        }
+    }
+}
