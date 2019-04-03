4728d2e200009ec64675a5b4dc53cd7741492f1f
omerlh <omerl@soluto.com>
2019-04-03 08:24:20 +0300

    run tests on the CI

---
+++ tests/crd-controller/run-tests.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly CT_VERSION=v2.2.0
+readonly KIND_VERSION=0.1.0
+readonly CLUSTER_NAME=e2e-test
+readonly K8S_VERSION=v1.13.2
+
+run_e2e_container() {
+    echo 'Running e2e container...'
+    docker run --rm --interactive --detach --network host --name e2e \
+        --volume "$(pwd):/workdir" \
+        --workdir /workdir \
+        "microsoft/dotnet:2.2-sdk" \
+        cat
+    echo
+}
+
+cleanup() {
+    echo 'Removing ct container...'
+    docker kill e2e > /dev/null 2>&1
+
+    echo 'Done!'
+}
+
+docker_exec() {
+    docker exec --interactive e2e "$@"
+}
+
+create_kind_cluster() {
+    echo 'Installing kind...'
+
+    curl -sSLo kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64"
+    chmod +x kind
+    sudo mv kind /usr/local/bin/kind
+
+    kind create cluster --name "$CLUSTER_NAME" --config test/crd-contoller/kind-config.yaml --image "kindest/node:$K8S_VERSION"
+
+    docker_exec mkdir -p /root/.kube
+
+    echo 'Copying kubeconfig to container...'
+    local kubeconfig
+    kubeconfig="$(kind get kubeconfig-path --name "$CLUSTER_NAME")"
+    docker cp "$kubeconfig" e2e:/root/.kube/config
+
+    docker_exec kubectl cluster-info
+    echo
+
+    echo -n 'Waiting for cluster to be ready...'
+    until ! grep --quiet 'NotReady' <(docker_exec kubectl get nodes --no-headers); do
+        printf '.'
+        sleep 1
+    done
+
+    echo '✔︎'
+    echo
+
+    docker_exec kubectl get nodes
+    echo
+
+    echo 'Cluster ready!'
+    echo
+}
+
+install_tiller() {
+    echo 'Installing Tiller...'
+    docker_exec kubectl --namespace kube-system create sa tiller
+    docker_exec kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    docker_exec helm init --service-account tiller --upgrade --wait
+    echo
+}
+
+install_hostpath-provisioner() {
+    # kind doesn't support Dynamic PVC provisioning yet, this is one way to get it working
+    # https://github.com/rimusz/charts/tree/master/stable/hostpath-provisioner
+
+    echo 'Installing hostpath-provisioner...'
+
+    # Remove default storage class. Will be recreated by hostpath -provisioner
+    docker_exec kubectl delete storageclass standard
+
+    docker_exec helm repo add rimusz https://charts.rimusz.net
+    docker_exec helm repo update
+    docker_exec helm install rimusz/hostpath-provisioner --name hostpath-provisioner --namespace kube-system --wait
+    echo
+}
+
+install_charts() {
+    docker_exec ct install
+    echo
+}
+
+main() {
+    run_ct_container
+    trap cleanup EXIT
+
+    create_kind_cluster
+}
+
+main
