ad48fe8182f7a581f232cfb49abf9088e02b8da3
omerlh <omerl@soluto.com>
2019-04-04 23:14:43 +0300

    push also controller image

--- ci/version_to_deploy.sh
+++ ci/version_to_deploy.sh
@@ -20,6 +20,16 @@ if [[ "$(git tag | grep -c "$ENCRYPTOR_API_TAG")" == "0" ]]; then
     export ENCRYPTOR_API_DOCKER_TAG=$ENCRYPTOR_API_TAG
 fi
 
+echo checking controller api version
+CONTROLLER_API_VERSION=$(grep -E "<Version>" ./src/crd-controller/crd-controller.csproj | grep -Eo "[0-9.]*(-rc[0-9]*)?")
+CONTROLLER_API_TAG="controller-$CONTROLLER_API_VERSION"
+export CONTROLLER_API_DOCKER_TAG="controller-latest"
+if [[ "$(git tag | grep -c "$CONTROLLER_API_TAG")" == "0" ]]; then
+    echo tagging "$CONTROLLER_API_TAG"
+    git tag "$CONTROLLER_API_TAG"
+    export CONTROLLER_API_DOCKER_TAG=$CONTROLLER_API_TAG
+fi
+
 echo  checking cli version
 CLI_VERSION=$(grep -E "\"version\"" ./cli/package.json  | grep -Eo "[0-9.]*(-rc[0-9]*)?")
 CLI_TAG="cli-$CLI_VERSION"
