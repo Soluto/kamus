bd81c551dd6a07d1290530af8c78e4cdb171eda9
omerlh <omerl@soluto.com>
2019-08-14 09:39:49 -0500

    pleaseeee

--- tests/crd-controller/run-tests.sh
+++ tests/crd-controller/run-tests.sh
@@ -43,7 +43,7 @@ create_kind_cluster() {
 
     docker cp kubectl e2e:/usr/local/bin/kubectl
 
-    if [[ $K8S_VERSION == "v.15.0" ]]
+    if [[ $K8S_VERSION == "v1.15.0" ]]
     then
         kind_config="kind-config-1.15.yaml"
     else
