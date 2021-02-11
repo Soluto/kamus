5f35bbfe71e493cd1457782163d873e293191194
Shai Katz <shai@soluto.com>
2021-02-11 17:50:50 +0200

    Controller is now HTTP only

--- src/crd-controller/Program.cs
+++ src/crd-controller/Program.cs
@@ -31,22 +31,6 @@ namespace CustomResourceDescriptorController
                         webBuilder.UseStartup<Startup>();
                         webBuilder.ConfigureKestrel((context, options) =>
                             {
-                                var tlsCertRootFolder = Environment.GetEnvironmentVariable("TLS_CERT_FOLDER");
-
-                                var cert = new X509Certificate2($"{tlsCertRootFolder}/certificate.crt");
-
-                                var rsa = RSA.Create();
-                                var content = File
-                                    .ReadAllText($"{tlsCertRootFolder}/privateKey.key")
-                                    .Replace("-----BEGIN RSA PRIVATE KEY-----", "")
-                                    .Replace("-----END RSA PRIVATE KEY-----", "")
-                                    .Replace("\n", "");
-                                rsa.ImportRSAPrivateKey(Convert.FromBase64String(content), out int bytesRead);
-
-                                cert = cert.CopyWithPrivateKey(rsa);
-
-                                options.Listen(IPAddress.Any, 8888, listenOptions => { listenOptions.UseHttps(cert); });
-
                                 options.Listen(IPAddress.Any, 9999, listenOptions => { });
                             })
                             ;
