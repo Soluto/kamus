4f5b8fd7e218035767841fb62d17507cc1d3ff77
amitaimazliah <amitaimazliah@gmail.com>
2021-11-10 16:09:48 +0200

    Fix crd tests to run local

--- tests/crd-controller/run-tests.sh
+++ tests/crd-controller/run-tests.sh
@@ -44,6 +44,14 @@ docker_exec() {
     docker exec --interactive e2e "$@"
 }
 
+build_docker_image() {
+  echo 'Building CRD controller docker image...'
+
+  docker build -t crd-controller --build-arg PROJECT_NAME=crd-controller .
+
+  echo 'Built CRD controller image successfully'
+}
+
 create_kind_cluster() {
     K8S_VERSION=$1
     echo 'Installing kind...'
@@ -61,7 +69,7 @@ create_kind_cluster() {
 
     TMPDIR=$HOME ./kind create cluster --name "$CLUSTER_NAME" --config tests/crd-controller/$kind_config --image "kindest/node:$K8S_VERSION"
 
-    ./kind load image-archive docker-cache-api/crd-controller.tar --name "$CLUSTER_NAME"
+    ./kind load docker-image crd-controller --name "$CLUSTER_NAME"
     docker_exec mkdir -p /root/.kube
 
     echo 'Copying kubeconfig to container...'
@@ -98,6 +106,7 @@ main() {
     run_e2e_container
     trap cleanup EXIT
 
+    build_docker_image
     create_kind_cluster "$1"
 
     run_test
